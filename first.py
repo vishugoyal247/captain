c6f9f47fdcd108cd5fa98ef1f2a1322f70b8895f
goalsachiever <69478693+goalsachiever@users.noreply.github.com>
2021-05-17 13:24:23 +0530

    Update first.py

--- first.py
+++ first.py
@@ -1,5 +1,5 @@
 filename=input("Enter the file name with the proper extension")
-if(filename[-3] == ".py"):
+if(filename[-3:] == ".py"):
   print("The file name is finded by the PYTHON")
 else:
   print("The file name is not the PYTHON ")
